532eea18849fe09fda0ead8a37f541a9c33566f7
arifaliasa <89333286+arifaliasa@users.noreply.github.com>
2023-10-20 16:14:18 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "2146ae1f-7d1d-4dbf-828b-54f9ca42f169"
+  client_id       = "63751d74-a47a-4236-82da-730543a10243"
+  client_secret   = "1I28Q~HcltVrUXdAo.KTQtLMHRhdCQeyeQnj5c4r"
+  tenant_id       = "f542f903-8530-4254-aa59-34aa2dcb3bc3"
+}
+
+resource "azurerm_resource_group" "example01" {
+    name = "kibb01-rg"
+    location = "eastus"
+}
+
+resource "azurerm_resource_group" "example02" {
+    name = "kibb02-rg"
+    location = "West Europe"
+}
